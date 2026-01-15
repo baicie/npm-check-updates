0bbe24e64bea66c16aa43adf58632197fc61ceb9
baicie <2055572879@qq.com>
2026-01-15 10:44:25 +0800

    release: ncu@0.1.23

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,27 +1,51 @@
 import { builtinModules } from 'node:module'
-import { defineConfig } from 'rolldown'
+import { ModuleFormat, RolldownOptions, defineConfig } from 'rolldown'
 import { dts } from 'rolldown-plugin-dts'
 import { visualizer } from 'rollup-plugin-visualizer'
 import pkg from './package.json'
 
 const inputs = ['./src/index.ts', './src/bin/cli.ts']
 
+const formats: ModuleFormat[] = []
+
+const external = [...builtinModules, ...builtinModules.map(m => `node:${m}`), ...Object.keys(pkg.dependencies)]
+
+const shardConfig: RolldownOptions = {
+  input: inputs,
+  treeshake: true,
+  transform: {
+    target: 'node18',
+  },
+  external,
+}
+
 export default defineConfig([
   {
-    input: inputs,
-    output: {
-      dir: './build',
-      format: 'esm',
-      sourcemap: true,
-      entryFileNames: `[name].js`,
-      chunkFileNames: 'chunks/[name].js',
-      exports: 'named',
-    },
-    treeshake: true,
-    transform: {
-      target: 'node18',
-    },
+    ...shardConfig,
+    output: [
+      {
+        dir: './dist/esm',
+        format: 'esm',
+        sourcemap: true,
+        entryFileNames: `[name].mjs`,
+        chunkFileNames: 'chunks/[name].mjs',
+        exports: 'named',
+      },
+    ],
     plugins: [dts({}), ...(process.env.ANALYZER ? [visualizer()] : [])],
-    external: [...builtinModules, ...builtinModules.map(m => `node:${m}`), ...Object.keys(pkg.dependencies)],
+  },
+  {
+    ...shardConfig,
+    output: [
+      {
+        dir: './dist/cjs',
+        format: 'cjs',
+        sourcemap: true,
+        entryFileNames: `[name].cjs`,
+        chunkFileNames: 'chunks/[name].cjs',
+        exports: 'named',
+      },
+    ],
+    plugins: [...(process.env.ANALYZER ? [visualizer()] : [])],
   },
 ])
